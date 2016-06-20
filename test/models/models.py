d18841fa82390bd6d2338b1287164f49932fd702
Vu Dinh Khoa <khoavic.uit@gmail.com>
2016-06-20 17:02:02 +0700

    add model Course

--- test/models/models.py
+++ test/models/models.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+class Course(models.Model):
+    _name = 'openacademy.course'
 
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
 # class test(models.Model):
 #     _name = 'test.test'
 

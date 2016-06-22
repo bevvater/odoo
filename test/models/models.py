6aeea1d99eccb887e2fbed84570bd947529bbffb
Vu Dinh Khoa <khoavic.uit@gmail.com>
2016-06-22 11:48:15 +0700

    update test

--- test/models/models.py
+++ test/models/models.py
@@ -3,7 +3,7 @@
 from openerp import models, fields, api
 
 class Course(models.Model):
-    _name = 'test'
+    _name = 'abc'
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
